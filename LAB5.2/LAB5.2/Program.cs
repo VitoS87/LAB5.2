c0d121f346634f637ca5de124d9dfda02b671193
Vitos <rustamminis@gmail.com>
2025-11-27 22:09:35 +0200

    Add files via upload

---
+++ LAB5.2/LAB5.2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите текст: ");
+string text1 = Console.ReadLine();
+string text2 = "";
+for (int i = 0; i < text1.Length; i++)
+{
+    if (text1[i] == '.')
+    {
+        text2 += "..";
+    }
+    else
+    {
+        text2 += text1[i];
+    }
+}
+Console.WriteLine(text2);
